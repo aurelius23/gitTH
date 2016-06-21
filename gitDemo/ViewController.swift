e935974217b95094e029ff549272eef2ceb6fbab
Giantad94 <gianluca.tadori@gmail.com>
2016-06-21 11:50:26 +0200

    Initial Commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Gianluca Tadori on 21/06/16.
+//  Copyright © 2016 Gianluca Tadori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
